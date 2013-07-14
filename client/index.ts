b12b12f43d262c5c526027072b526bcda1226d93
James Hudon <jameshudon@gmail.com>
2013-07-14 14:10:35 -0400

    add aeson and json conversion on the server side

--- client/index.ts
+++ client/index.ts
@@ -6,4 +6,4 @@ $(function() {
 
   var user = "Jane User";
 
-})
+});
--- src/Main.hs
+++ src/Main.hs
@@ -10,11 +10,21 @@ import           Control.Monad.IO.Class
 import           System.Process (runInteractiveProcess, ProcessHandle)
 import qualified System.IO as IO
 import           System.IO.Error (isEOFError)
-import qualified Data.ByteString.Char8 as C
-import           Data.Maybe
 import qualified Control.Exception as E
+import           Data.Aeson (ToJSON, encode, (.=), object, toJSON)
+import           Data.Text (Text)
+import           Data.ByteString.Lazy (toStrict)
+import           Data.ByteString.UTF8 (fromString, toString)
 
 type ProcHandles = (IO.Handle, IO.Handle, IO.Handle, ProcessHandle)
+
+data RPC = RPC
+  { func :: Text
+  , params :: [Text]
+  } deriving Show
+
+instance ToJSON RPC where
+  toJSON (RPC f ps) = object ["method" .= f, "params" .= ps]
 
 main :: IO ()
 main = do
@@ -28,7 +38,7 @@ main = do
 site :: ProcHandles  -> Snap ()
 site rProc =
     ifTop (serveDirectory "static") <|>
-    route [ ("send-cmd", cmdHandler rProc)
+    route [ ("R-rand-vec", randVecHandler rProc)
           ] <|>
     dir "static" (serveDirectory "static")
 
@@ -47,27 +57,23 @@ hGetLines hin = do
       return (line:ls)
 
 
--- TODO: restart R proc on fail
+-- If stdout has nothing, returns what's in stderr...
+-- We're assuming the R RPC proc returns 1 line of results.
+getResults :: IO.Handle -> IO.Handle -> IO String
+getResults hout herr = do
+  o <- E.try $ IO.hGetLine hout
+  case o of
+    Left e -> if isEOFError e then hGetLines herr >>= return . unlines else E.ioError e
+    Right line -> return line
 
--- Call an R function with the given param and return verbatim response
--- Only 0-255 of Unicode is supported (Char8)
-cmdHandler :: ProcHandles -> Snap ()
-cmdHandler (rin, rout, rerr, _) = do
-  cmd <- getParam "cmd"
-  res <- liftIO $ do
-    let c = C.append (fromJust cmd) (C.pack "\n")
-    -- Send cmd to R proc and get result
-    IO.hPutStr rin $ C.unpack c
-    IO.hFlush rin
-    getResults rout rerr
-  writeBS $ C.pack res
-  where
-    -- If stdout has nothing, returns what's in stderr...
-    -- We're assuming the R RPC proc returns 1 line of results.
-    getResults :: IO.Handle -> IO.Handle -> IO String
-    getResults hout herr = do
-      o <- E.try $ IO.hGetLine hout
-      case o of
-        Left e -> if isEOFError e then hGetLines herr >>= return . unlines else E.ioError e
-        Right line -> return line
 
+randVecHandler :: ProcHandles -> Snap ()
+randVecHandler r = liftIO (rRPC r) >>= writeBS . fromString
+
+-- aeson encodes to a lazy bytestring, so we make it strict and use
+-- utf8-string's from and to String functions
+rRPC :: ProcHandles -> IO String
+rRPC (hin, hout, herr, _) = do
+  IO.hPutStrLn hin . toString . toStrict $ encode (RPC {func = "fib", params = ["12"]})
+  IO.hFlush hin
+  getResults hout herr
