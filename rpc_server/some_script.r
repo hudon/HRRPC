9d52101b306758c629773f1dbde51d36f86c8347
James Hudon <jameshudon@gmail.com>
2013-07-13 23:38:55 -0400

    remove redundant file

--- rpc_server/some_script.r
+++ rpc_server/some_script.r
@@ -1,11 +1,11 @@
 require( "stats", quietly = TRUE )
 
 user_func <- function( times ) {
-	rep( "I am made of sugar", times )
+  rep( "I am a teapot.", times )
 }
 
 fib <- function( n ) {
-	if( n < 2 )
-		return( 1 )
-	return( fib( n - 1 ) + fib( n - 2 ) )
+  if( n < 2 )
+    return( 1 )
+  return( fib( n - 1 ) + fib( n - 2 ) )
 }
