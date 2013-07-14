7a8288d16faf3acbd15e75dcd9edaa3697442685
James Hudon <jameshudon@gmail.com>
2013-07-14 17:07:27 -0400

    pull result out of JSON on client side
    TODO: pull result out of result on server side
    TODO: use AngularJS instead of jQuery on client side
    TODO: setup minimal dev env using Shake and Shelly

--- client/index.ts
+++ client/index.ts
@@ -1,17 +1,27 @@
 /// <reference path="jquery.d.ts" />
+
+interface RPCResult {
+  HRRPC: string;
+  result: any;
+}
+
+function parseRPCResult (res: string): RPCResult {
+  return JSON.parse(res);
+}
+
 $(function() {
 
   $('#R-rand-vec').submit(function (e) {
-    $.get('R-rand-vec', function (res) {
-      $('#R-rand-vec-result').text(res);
+    $.get('R-rand-vec', function (res: string) {
+      $('#R-rand-vec-result').text(parseRPCResult(res).result);
     });
     e.preventDefault();
     return false;
   });
 
   $('#R-fib').submit(function (e) {
-    $.get('R-fib', $(this).serialize(), function (res) {
-      $('#R-fib-param-result').text(res);
+    $.get('R-fib', $(this).serialize(), function (res: string) {
+      $('#R-fib-param-result').text(parseRPCResult(res).result);
     });
     e.preventDefault();
     return false;
